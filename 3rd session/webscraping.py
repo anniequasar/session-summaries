555eb7281fd492611d65b21548dc24dca6181b71
anniequasar <32969048+anniequasar@users.noreply.github.com>
2019-01-17 18:47:58 +1000

    Update webscraping.py

--- 3rd session/webscraping.py
+++ 3rd session/webscraping.py
@@ -1,46 +1,25 @@
 from bs4 import BeautifulSoup
 
-
-
 from urllib.request import urlopen
 
-
-
 page = urlopen("https://spreets.com.au/deals/sydney")
-
-
 
 soup = BeautifulSoup(page, features="html.parser")
 
 
-
-# a for loop that prints all the deals greater than 70% and it's link from the website onto a new line
-
-
+# a for loop that prints all the deals greater than 70% and its link from the website onto a new line
 
 divlist = soup.find_all("div", "deal-list-wrapper")
 
-
-
 for div in divlist:
-
-
 
     em = div.em
 
-
-
     a = div.a
-
-
 
     if em and "%" in em.string:
 
-
-
         percentage = int(em.string[6:8])
-
-
 
         if percentage > 70:
 
