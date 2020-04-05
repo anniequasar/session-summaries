0065205c384ab599a887c324c7dbcfce1095531b
D Tim Cummings <tim@triptera.com.au>
2020-04-05 22:16:58 +1000

    Convert column from object to int so don’t need to create extra float columns for np.log2 to work

--- TCB/meetup053_Tim_plotly_covid_19_doubling_rate.py
+++ TCB/meetup053_Tim_plotly_covid_19_doubling_rate.py
@@ -36,7 +36,7 @@ fig = {
 # implement plotly.js in colab and display our interactive chart (try hovering and clicking)
 # plotly.io.show(fig)
 # If you are not using interactive python you can create an html file and open it
-plotly.io.write_html(fig, "demo1.html")
+plotly.io.write_html(fig, "demo1.html", auto_open=True)
 
 # Challenge 1: Given the following lists of x and y values which represent sigmoid function
 # Plot the values in a scatter chart using dict 
@@ -206,7 +206,7 @@ print(f"series_sum.index after : {series_sum.index}")
 # Solution to challenge 6
 def series_sum_for_location(df, country=None, state=None):
     df = df_for_location(df=df, state=state, country=country)
-    series = df.sum(axis="index")[4:]
+    series = df.sum(axis="index")[4:].astype(int)
     series.index = pd.to_datetime(series.index)
     return series
 
@@ -294,15 +294,13 @@ plot_for_location(df_confirmed, description="Confirmed").write_html("challenge9.
 averaged_days = 3
 idx_start = series_sum.index[series_sum >= 100][0]
 df_sum = pd.DataFrame(series_sum, columns=['current'])
-df_sum['previous'] = df_sum['current'].shift(averaged_days)
+df_sum['previous'] = df_sum['current'].shift(averaged_days, fill_value=0)
 df_plot = df_sum.loc[idx_start:].copy()  # need .copy() or will get warning later on
 print("shifted results")
 print(df_plot)
 
 # Calculate how many days to double
-df_plot['flt_current'] = df_plot['current'].astype(float)  # need floats for np.log2 to work
-df_plot['flt_previous'] = df_sum['current'].shift(averaged_days).astype(float)
-df_plot['doubling days'] = averaged_days / np.log2(df_plot['flt_current'] / df_plot['flt_previous'])
+df_plot['doubling days'] = averaged_days / np.log2(df_plot['current'] / df_plot['previous'])
 print("calculated doubling days")
 print(df_plot)
 
@@ -316,10 +314,9 @@ def plot_doubling_for_location(df, country=None, state=None, num_start=100, desc
     series_sum = series_sum_for_location(df, country=country, state=state)
     idx_start = series_sum.index[series_sum >= num_start][0]
     df_sum = pd.DataFrame(series_sum, columns=['current'])
+    df_sum['previous'] = df_sum['current'].shift(averaged_days, fill_value=0)
     df_plot = df_sum.loc[idx_start:].copy()
-    df_plot['flt_current'] = df_plot['current'].astype(float)  # need floats for np.log2 to work
-    df_plot['flt_previous'] = df_sum['current'].shift(averaged_days).astype(float)
-    df_plot['doubling days'] = averaged_days / np.log2(df_plot['flt_current'] / df_plot['flt_previous'])
+    df_plot['doubling days'] = averaged_days / np.log2(df_plot['current'] / df_plot['previous'])
     location = location_name(country=country, state=state)
     fig = go.Figure(
         data=[go.Scatter(x=df_plot.index, y=df_plot['doubling days'], name=location, showlegend=True, mode="lines")],
@@ -339,10 +336,9 @@ def plot_for_location(df, country=None, state=None, num_start=100, description="
     series_sum = series_sum_for_location(df, country=country, state=state)
     idx_start = series_sum.index[series_sum >= num_start][0]
     df_sum = pd.DataFrame(series_sum, columns=['current'])
+    df_sum['previous'] = df_sum['current'].shift(averaged_days, fill_value=0)
     df_plot = df_sum.loc[idx_start:].copy()
-    df_plot['flt_current'] = df_plot['current'].astype(float)  # need floats for np.log2 to work
-    df_plot['flt_previous'] = df_sum['current'].shift(averaged_days).astype(float)
-    df_plot['doubling days'] = averaged_days / np.log2(df_plot['flt_current'] / df_plot['flt_previous'])
+    df_plot['doubling days'] = averaged_days / np.log2(df_plot['current'] / df_plot['previous'])
     location = location_name(country=country, state=state)
     fig = plotly.subplots.make_subplots(
         rows=3, cols=1, shared_xaxes=True,
@@ -387,10 +383,9 @@ def plot_for_location(df, country=None, state=None, num_start=100, description="
     series_sum = series_sum_for_location(df, country=country, state=state)
     idx_start = series_sum.index[series_sum >= num_start][0]
     df_sum = pd.DataFrame(series_sum, columns=['current'])
+    df_sum['previous'] = df_sum['current'].shift(averaged_days, fill_value=0)
     df_plot = df_sum.loc[idx_start:].copy()
-    df_plot['flt_current'] = df_plot['current'].astype(float)  # need floats for np.log2 to work
-    df_plot['flt_previous'] = df_sum['current'].shift(averaged_days).astype(float)
-    df_plot['doubling days'] = averaged_days / np.log2(df_plot['flt_current'] / df_plot['flt_previous'])
+    df_plot['doubling days'] = averaged_days / np.log2(df_plot['current'] / df_plot['previous'])
     location = location_name(country=country, state=state)
     fig = plotly.subplots.make_subplots(
         rows=3, cols=1, shared_xaxes=True,
